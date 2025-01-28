2aaff02f3c871d657c81c0d8bb36ace9c54e656d
Ksenia Ivanchenko <k.ivanchenko20@gmail.com>
2025-01-29 00:46:02 +0700

    delete root.tsx

--- src/pages/root.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from 'react';
-import { Button } from '../ui-kit';
-
-export const RootPage: FC = () => {
-  return <Button styleType={{ type: 'primary', variant: 'contained' }}></Button>;
-};
