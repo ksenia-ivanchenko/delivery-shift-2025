2ee1a0efaa826d20a1f3ec374ece0f195207ba27
Ksenia Ivanchenko <k.ivanchenko20@gmail.com>
2025-01-29 11:57:20 +0700

    добавлен баннер на MainPage

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -6,10 +6,12 @@ export const MainPage = () => {
     <main className={styles.content}>
       <div className={styles.description}>
         <h1 className={styles.title}>Мы доставим ваш заказ</h1>
-        <p className={styles.text}>Отправляйте посылки в приложении Шифт Delivery</p>
+        <p className={styles.text}>
+          Отправляйте посылки <br /> в приложении Шифт Delivery
+        </p>
         <div className={styles.qrContainer}>
-          <BoxIcon />
-          <QRCode />
+          <BoxIcon width={64} height={64} />
+          <QRCode width={64} height={64} />
           <p className={styles.qrText}>Наведите камеру телефона на QR‑код</p>
         </div>
       </div>
